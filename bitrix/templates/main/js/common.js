2ad3d6548874abe67490a94e8cef7ce97e5db423
Tim <andrei.kurlyandchik@gmail.com>
2017-05-17 17:50:32 +0300

    add ajax

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -60,11 +60,25 @@ function initFile() {
     });
 }
 
+function initMouseMove() {
+    var movementStrength = 20;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $(".container-wrapper").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 5;
+        var newvalueY = height * pageY * -1 - 5;
+        $('.mouseov').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    })
+}
+
 focusInput();
 scrollUp();
 autoTextarea();
 scrollForm();
 initFile();
+initMouseMove();
 
 //Parallax
 if ($('.parallax').length) {
